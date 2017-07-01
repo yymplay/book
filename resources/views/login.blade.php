5c8b66cebf68e8b077e8471380a6b2ce941af0d3
yym <749092928@qq.com>
2017-07-02 02:32:25 +0800

    .

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,14 @@
+<!-- 继承master模板 -->
 @extends('master')
 
 @section('title','登录')
+
 @section('content')
 <h1>这是登录页面</h1>
+@endsection
+
+@section('my-js')
+<script>
+	alert('这是登录页面???');
+</script>
 @endsection
