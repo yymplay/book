48679201e53647d755d61256477843755f52a4b3
yym <749092928@qq.com>
2017-07-02 02:58:12 +0800

    .

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,23 +9,71 @@
 </head>
 <body>
 	@yield('content')
-	<div class="weui_msg">
-    <div class="weui_icon_area"><i class="weui_icon_success weui_icon_msg"></i></div>
-    <div class="weui_text_area">
-        <h2 class="weui_msg_title">操作成功</h2>
-        <p class="weui_msg_desc">内容详情，可根据实际需要安排</p>
+	<!-- tooltips -->
+<div class="bk_toptips"><span></span></div>
+
+<div id="global_menu" onclick="onMenuClick();">
+  <div></div>
+</div>
+
+<!--BEGIN actionSheet-->
+<div id="actionSheet_wrap">
+    <div class="weui_mask_transition" id="mask"></div>
+    <div class="weui_actionsheet" id="weui_actionsheet">
+        <div class="weui_actionsheet_menu">
+            <div class="weui_actionsheet_cell" onclick="onMenuItemClick(1)">用户中心</div>
+            <div class="weui_actionsheet_cell" onclick="onMenuItemClick(2)">选择套餐</div>
+            <div class="weui_actionsheet_cell" onclick="onMenuItemClick(3)">周边油站</div>
+            <div class="weui_actionsheet_cell" onclick="onMenuItemClick(4)">常见问题</div>
+        </div>
+        <div class="weui_actionsheet_action">
+            <div class="weui_actionsheet_cell" id="actionsheet_cancel">取消</div>
+        </div>
     </div>
-    <div class="weui_opr_area">
-        <p class="weui_btn_area">
-            <a href="javascript:;" class="weui_btn weui_btn_primary">确定</a>
-            <a href="javascript:;" class="weui_btn weui_btn_default">取消</a>
-        </p>
-    </div>
-    <div class="weui_extra_area">
-        <a href="">查看详情</a>
-    </div>
+</div>
 </div>
 </body>
 <script src="/js/jquery-1.11.2.min.js"></script>
+<script type="text/javascript">
+function hideActionSheet(weuiActionsheet, mask) {
+    weuiActionsheet.removeClass('weui_actionsheet_toggle');
+    mask.removeClass('weui_fade_toggle');
+    weuiActionsheet.on('transitionend', function () {
+        mask.hide();
+    }).on('webkitTransitionEnd', function () {
+        mask.hide();
+    })
+}
+
+function onMenuClick () {
+    var mask = $('#mask');
+    var weuiActionsheet = $('#weui_actionsheet');
+    weuiActionsheet.addClass('weui_actionsheet_toggle');
+    mask.show().addClass('weui_fade_toggle').click(function () {
+        hideActionSheet(weuiActionsheet, mask);
+    });
+    $('#actionsheet_cancel').click(function () {
+        hideActionSheet(weuiActionsheet, mask);
+    });
+    weuiActionsheet.unbind('transitionend').unbind('webkitTransitionEnd');
+}
+
+function onMenuItemClick(index) {
+  var mask = $('#mask');
+  var weuiActionsheet = $('#weui_actionsheet');
+  hideActionSheet(weuiActionsheet, mask);
+  if(index == 1) {
+
+  } else if(index == 2) {
+
+  } else if(index == 3){
+
+  } else {
+    $('.bk_toptips').show();
+    $('.bk_toptips span').html("敬请期待!");
+    setTimeout(function() {$('.bk_toptips').hide();}, 2000);
+  }
+}
+</script>
 @yield('my-js')
 </html>
