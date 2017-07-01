0fee27e7de0bb7ff87980da09708b7e0d00e1122
yym <749092928@qq.com>
2017-07-02 02:49:02 +0800

    .

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,11 +3,19 @@
 <head>
 	<meta charset="UTF-8">
 	<title>@yield('title')</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<link rel="stylesheet" href="/css/weui.css">
 	<link rel="stylesheet" href="/css/book.css">
 </head>
 <body>
 	@yield('content')
+	<div id="toast" style="display: none;">
+	    <div class="weui_mask_transparent"></div>
+	    <div class="weui_toast">
+	        <i class="weui_icon_toast"></i>
+	        <p class="weui_toast_content">已完成</p>
+	    </div>
+	</div>
 </body>
 <script src="/js/jquery-1.11.2.min.js"></script>
 @yield('my-js')
