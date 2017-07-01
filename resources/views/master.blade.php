b4b02f2c57609622b0428bb7f6f13d04932e2f02
yym <749092928@qq.com>
2017-07-02 02:51:05 +0800

    .

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,13 +9,22 @@
 </head>
 <body>
 	@yield('content')
-	<div id="toast" style="display: none;">
-	    <div class="weui_mask_transparent"></div>
-	    <div class="weui_toast">
-	        <i class="weui_icon_toast"></i>
-	        <p class="weui_toast_content">已完成</p>
-	    </div>
-	</div>
+	<div class="weui_msg">
+    <div class="weui_icon_area"><i class="weui_icon_success weui_icon_msg"></i></div>
+    <div class="weui_text_area">
+        <h2 class="weui_msg_title">操作成功</h2>
+        <p class="weui_msg_desc">内容详情，可根据实际需要安排</p>
+    </div>
+    <div class="weui_opr_area">
+        <p class="weui_btn_area">
+            <a href="javascript:;" class="weui_btn weui_btn_primary">确定</a>
+            <a href="javascript:;" class="weui_btn weui_btn_default">取消</a>
+        </p>
+    </div>
+    <div class="weui_extra_area">
+        <a href="">查看详情</a>
+    </div>
+</div>
 </body>
 <script src="/js/jquery-1.11.2.min.js"></script>
 @yield('my-js')
